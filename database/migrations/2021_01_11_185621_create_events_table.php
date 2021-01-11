bddb42b749651b52a64e6e20070fc56a4fdbcc26
Naquib <seyyednaquib@graduate.utm.my>
2021-01-12 03:05:13 +0800

    Migrate data user and event

---
+++ database/migrations/2021_01_11_185621_create_events_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->integer('Event_id');
+            $table->string('Event_name');
+            $table->string('Report_desc');
+            $table->date('date_create');
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
