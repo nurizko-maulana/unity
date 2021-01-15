bc6a8c7643215413321117588e7b753824aff40f
Maulana <duncanleo@192.168.0.103>
2021-01-15 08:14:00 +0700

    add report controller

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -3,8 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Report;
 
 class ReportController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $todos = Report::all();
+        return view('todo.index', [
+            'todos' => $todos
+        ]);
+    }
 }
